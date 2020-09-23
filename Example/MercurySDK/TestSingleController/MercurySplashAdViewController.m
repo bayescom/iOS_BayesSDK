322461e59534cac91c8a1679af1bc7142874b82f
CherryKing <455153666@qq.com>
2020-09-23 13:42:03 +0800

    update to 3.1.5

--- Example/MercurySDK/TestSingleController/MercurySplashAdViewController.m
+++ Example/MercurySDK/TestSingleController/MercurySplashAdViewController.m
@@ -29,16 +29,11 @@
         @{@"addesc": @"开屏(schemaLink)", @"adspotId": @"10002620"},
         @{@"addesc": @"开屏(universalLink)", @"adspotId": @"10002621"},
         
-//        @{@"addesc": @"开屏无广告返回", @"adspotId": @"10000101"},
-//        @{@"addesc": @"开屏数据解析失败", @"adspotId": @"10000102"},
-//        @{@"addesc": @"开屏adtype匹配错误", @"adspotId": @"10000103"},
-//        @{@"addesc": @"开屏creative_type匹配错误", @"adspotId": @"10000104"},
-//        @{@"addesc": @"开屏网络超时", @"adspotId": @"10000105"},
-//        @{@"addesc": @"开屏素材加载失败", @"adspotId": @"10000106"},
-//        @{@"addesc": @"开屏长图", @"adspotId": @"10000001"},
-//        @{@"addesc": @"开屏短图", @"adspotId": @"10000002"},
-//        @{@"addesc": @"开屏Gif", @"adspotId": @"10000003"},
-//        @{@"addesc": @"开屏视频", @"adspotId": @"10000004"},
+        @{@"addesc": @"富媒体开屏-Gallery", @"adspotId": @"10003412"},
+        @{@"addesc": @"富媒体开屏-sliding", @"adspotId": @"10003411"},
+        @{@"addesc": @"富媒体开屏-Cube", @"adspotId": @"10003409"},
+        @{@"addesc": @"富媒体开屏-Swipe", @"adspotId": @"10003408"},
+        
     ];
     self.btn1Title = @"加载并显示广告";
 }
