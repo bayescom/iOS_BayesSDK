92b23cb3f25731137a9147cc577406b2516d8b9e
shichuanyang <shicy@bayescom.com>
2022-09-06 15:14:47 +0800

    release 3.1.7.2

--- Example/MercurySDK/MERCURYAppDelegate.m
+++ Example/MercurySDK/MERCURYAppDelegate.m
@@ -58,62 +58,16 @@
 
 - (void)splashShow {   // 开屏
     // 设置AppId MediaKey
-//    [MercuryConfigManager setAppID:@"100255"
-//                     appKey:@"757d5119466abe3d771a211cc1278df7"];
+    [MercuryConfigManager setAppID:@"100255"
+                     appKey:@"757d5119466abe3d771a211cc1278df7"];
     
-    [MercuryConfigManager setAppID:@"101859"
-                          appKey:@"9fd708c28a14006344301ef5cac76adb"
-                            config:@{}];
+//    [MercuryConfigManager setAppID:@"101859"
+//                          appKey:@"9fd708c28a14006344301ef5cac76adb"
+//                            config:@{}];
 
     
     // 开启日志
     [MercuryConfigManager openDebug:YES];
-    // 支持预缓存资源
-    [MercuryConfigManager preloadedResourcesIfNeed:YES];
-    NSLog(@"%@", [MercuryConfigManager sdkVersion]);
-    _ad = [[MercurySplashAd alloc] initAdWithAdspotId:@"10002436" delegate:nil];
-    _ad.controller = self.window.rootViewController;
-    _ad.delegate = self;
-    _ad.placeholderImage = [UIImage imageNamed:@"LaunchImage_img"];
-    _ad.logoImage = [UIImage imageNamed:@"app_logo"];
-    [_ad loadAdAndShow];
-}
-
-// MARK: ======================= MercurySplashAdDelegate =======================
-- (void)mercury_splashAdDidLoad:(MercurySplashAd *)splashAd {
-    NSLog(@"开屏广告模型加载成功 %s", __func__);
-}
-
-- (void)mercury_splashAdSuccessPresentScreen:(MercurySplashAd *)splashAd {
-    NSLog(@"开屏广告成功曝光 %s", __func__);
-}
-
-- (void)mercury_splashAdFailError:(NSError *)error {
-    NSLog(@"开屏广告曝光失败 %s %@", __func__, error);
-}
-
-- (void)mercury_splashAdLifeTime:(NSUInteger)time {
-    NSLog(@"开屏广告剩余时间回调 %s _ %ld", __func__, time);
-}
-
-- (void)mercury_splashAdApplicationWillEnterBackground:(MercurySplashAd *)splashAd {
-    NSLog(@"应用进入后台时回调 %s", __func__);
-}
-
-- (void)mercury_splashAdExposured:(MercurySplashAd *)splashAd {
-    NSLog(@"开屏广告曝光回调 %s", __func__);
-}
-
-- (void)mercury_splashAdClicked:(MercurySplashAd *)splashAd {
-    NSLog(@"开屏广告点击回调 %s", __func__);
-}
-
-- (void)mercury_splashAdWillClosed:(MercurySplashAd *)splashAd {
-    NSLog(@"开屏广告将要关闭回调 %s", __func__);
-}
-
-- (void)mercury_splashAdClosed:(MercurySplashAd *)splashAd {
-    NSLog(@"开屏广告关闭回调 %s", __func__);
 }
 
 @end
